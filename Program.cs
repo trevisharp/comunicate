5acaf92f316c518d2a8aa4dc6f569afeaf2bcd63
Trevisan <leonardosilio@alunos.utfpr.edu.br>
2023-05-10 01:51:01 -0300

    tests to get public ip

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+using static System.Console;
+
+var ip = await getPublicIP();
+WriteLine(ip);
+
+async Task<string> getPublicIP()
+{
+    HttpClient http = new HttpClient();
+    var response = await http.GetAsync("https://api.ipify.org");
+    return await response.Content.ReadAsStringAsync();
+}
